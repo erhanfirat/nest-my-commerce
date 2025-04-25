49a1d18914df876abff663f6e81d7a526c3da5a1
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-04-25 09:15:19 +0300

    db connection implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { OrderModule } from './order/order.module';
 import { PaymentModule } from './payment/payment.module';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './common/shared.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,6 +16,18 @@ import { SharedModule } from './common/shared.module';
     OrderModule,
     PaymentModule,
     AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('POSTGRES_HOST'),
+        port: config.get<number>('POSTGRES_PORT'),
+        username: config.get<string>('POSTGRES_USER'),
+        password: config.get<string>('POSTGRES_PASSWORD'),
+        database: config.get<string>('POSTGRES_NAME'),
+      }),
+    }),
   ],
 })
 export class AppModule {}
