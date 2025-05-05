c0d4537fec30428f787a8046a47028f7a4549975
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-05-05 22:50:56 +0300

    stock added to product res dto

--- src/products/dto/product-response.dto.ts
+++ src/products/dto/product-response.dto.ts
@@ -6,12 +6,14 @@ export class ProductResponseDto {
   description: string;
   price: number;
   images: string[];
+  stock: number;
 
   constructor(product: Product) {
     this.id = product.id;
     this.name = product.name;
     this.description = product.description;
     this.price = product.price;
+    this.stock = product.stock;
     this.images = product.images?.map((img) => img.url); // örneğin sadece url dönüyoruz
   }
 }
