3063ef97cbe5c1e94fdb0c1c6be06980d83c95f4
Erhan FIRAT <erhan.firat@cytosmart.com>
2025-05-08 18:08:32 +0300

    user visit mongo conf added

--- src/user-visit/user-visit.module.ts
+++ src/user-visit/user-visit.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserVisitService } from './user-visit.service';
 import { UserVisitController } from './user-visit.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserVisit, UserVisitSchema } from './schemas/user-visit.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: UserVisit.name, schema: UserVisitSchema },
+    ]),
+  ],
   providers: [UserVisitService],
-  controllers: [UserVisitController]
+  controllers: [UserVisitController],
+  exports: [UserVisitService],
 })
 export class UserVisitModule {}
